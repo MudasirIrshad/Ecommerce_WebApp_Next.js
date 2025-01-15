fa627d866548d1965f5f5e7a140d4e6459a7980d
Mudasir Irshad <mudasirirshad47@gmail.com>
2025-01-16 00:21:08 +0500

    signup

--- my-turborepo/packages/ui/components/Signup.tsx
+++ my-turborepo/packages/ui/components/Signup.tsx
@@ -1,10 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box, Button, TextField } from "@mui/material";
 
 export function Signup() {
+  const handleClick = ()=>{
+    alert('clicked')
+  }
+  const [username, setUsername] = useState("");
+  const [gmail, setGmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <>
-      <input type="text" placeholder="email"></input>
-      <input type="text" placeholder="password"></input>
-    </>
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <Box
+        component="section"
+        sx={{
+          p: 2,
+          border: "1px dashed grey",
+          width: "400px",
+          margin: "10px",
+          padding: "10px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <TextField
+          required
+          style={{ padding: "10px", width: "350px" }}
+          id="outlined-required"
+          label="Name"
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+        <TextField
+          required
+          style={{ padding: "10px", width: "350px" }}
+          id="outlined-required"
+          label="Gmail"
+          onChange={(e) => {
+            setGmail(e.target.value);
+          }}
+        />
+        <TextField
+          required
+          style={{ padding: "10px", width: "350px" }}
+          id="outlined-required"
+          label="Password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <Button
+          sx={{ backgroundColor: "black", width: "350px", margin: "10px" }}
+          variant="contained"
+          onClick={handleClick}
+        >
+          Signup
+        </Button>
+      </Box>
+    </div>
   );
 }
