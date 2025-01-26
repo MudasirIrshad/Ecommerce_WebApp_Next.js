dc0a5c4092ca6eacd85f69ee0ad8bb1483ec5d89
MudasirIrshad <mudasirirshad47@gmail.com>
2025-01-26 23:53:24 +0500

    working on role base login

--- my-turborepo/packages/ui/components/Navbar.tsx
+++ my-turborepo/packages/ui/components/Navbar.tsx
@@ -70,7 +70,7 @@ export default function Navbar() {
                 marginRight: "5px",
               }}
             >
-              Courses
+              Products
             </Button>
           </>
         ) : (
